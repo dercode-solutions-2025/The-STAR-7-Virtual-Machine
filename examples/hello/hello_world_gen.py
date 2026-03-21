604bedbef283e969e203d22dba446ef0484916dd
nobody_on_earth <reddeadarthurfan1889@gmail.com>
2026-03-21 14:10:07 -0500

    Create hello_world_gen.py

---
+++ examples/hello/hello_world_gen.py
@@ -0,0 +1,18 @@
+text = "Hello World"
+x = []
+def main(text):
+	print("{")
+	text = list(text)
+	for item in text:
+		for i in range(ord(item)):
+			print('"0++", ')
+			x.append("0++")
+		print('"6==", ')
+		x.append("6==")
+		print('"6++", ')
+	print('"2=="')
+	x.append("2==")
+	print("}")
+main("Hello World!")
+for item in x:
+	print(item)
