4c88dba9cdfba1f42b6985539e09ae71ac5e8bc9
nobody_on_earth <reddeadarthurfan1889@gmail.com>
2026-03-23 16:38:39 -0500

    Update svm.hpp

--- svm/svm.hpp
+++ svm/svm.hpp
@@ -1,14 +1,18 @@
 #include "utils.hpp"
 #include "execs.hpp"
+#include "files.hpp"
 #include <string>
 #include <list>
 #ifndef SVM_HPP
 #define SVM_HPP
 namespace svm {
-	void _main(std::list<std::string> commands) {
+	void main_comms(std::list<std::string> commands) {
 		for (std::string inst : commands) {
 			execs::exec_single(inst);
 		}
+	}
+	auto main_script(const std::string path) {
+		files::exec_script(path);
 	}
 }
 #endif
